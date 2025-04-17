66fea810ff323d38013e63ab95a8dc85d012d818
arya87078 <vp643729@gmail.com>
2025-04-17 12:25:11 +0000

    container confic done

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,26 @@
+const name="Vishal"
+const repoCount =50
+
+console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName =new String ('vishal-hc-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString1= "      Vishal       "
+console.log(newString1);
+console.log(newString1.trim());
