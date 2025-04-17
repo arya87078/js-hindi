27d98fabc1e8fa6404ecba409e2a6d7ef47e6338
arya87078 <vp643729@gmail.com>
2025-04-17 09:02:42 +0000

    container confic done

---
+++ .devcontainer/01_basics/function.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    console.log("Hello, " + name);
+};
+greet("world");  // Output: Hi, Bob
+
+function sayHeloo() {
+    console.log("hello world!");
+    
+}
+    
+sayHeloo();
+sayHeloo();
+
---
+++ .devcontainer/01_basics/let,var,const.js
@@ -0,0 +1,15 @@
+const accountId = 14452
+let accountEmail="vihsal123@gmail.com"
+var accountPassword ="12345"
+accountCity ="Delhi"
+let accountState;
+
+
+accountEmail="vp64@gmail.com"
+accountPassword="2121212"
+accountCity="bengaluru"
+console.log(accountId);
+console.log(accountId,accountEmail,accountPassword,accountCity,accountState);
+
+
+
